ed6176a83394d518e4d1e32d0d3f86b4be246e0d
Kharanshu25b03110018 <kharanshu.25b03110018@abes.ac.in>
2026-06-19 09:56:34 +0530

    End term solutions.

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Selecting elements
-let heading = document.getElementById("heading");
-let input = document.getElementById("inputText");
-let para = document.getElementById("para");
-// Buttons
-let changeTextBtn = document.getElementById("changeTextBtn");
-let colorBtn = document.getElementById("colorBtn");
-let fontBtn = document.getElementById("fontBtn");
-let toggleBtn = document.getElementById("toggleBtn");
-let resetBtn = document.getElementById("resetBtn");
-// 1. Change Heading Text (onclick)
-changeTextBtn.onclick = function() {
-    heading.innerText = input.value;
-};
-// 2. Change Background Color (addEventListener)
-colorBtn.addEventListener("click", function() {
-    document.body.style.backgroundColor = "lightblue";
-});
-// 3. Change Font Size (onmouseover)
-fontBtn.onmouseover = function() {
-    heading.style.fontSize = "40px";
-};
-// 4. Show/Hide Paragraph
-let isVisible = true;
-toggleBtn.addEventListener("click", function() {
-    if (isVisible) {
-        para.style.display = "none";
-        isVisible = false;
-    } else {
-        para.style.display = "block";
-        isVisible = true;
+function calculateAge() {
+
+    let dob = document.getElementById("dob").value;
+    let error = document.getElementById("error");
+    let result = document.getElementById("result");
+
+    error.innerHTML = "";
+
+    if (dob === "") {
+        error.innerHTML = "Please select your Date of Birth";
+        result.innerHTML = "Your age will appear here";
+        return;
     }
-});
-// 5. Input Change Event (onchange)
-input.onchange = function() {
-    console.log("Input changed to: " + input.value);
-};
-// 6. Reset Page
-resetBtn.addEventListener("click", function() {
-    heading.innerText = "Welcome to JavaScript DOM";
-    document.body.style.backgroundColor = "white";
-    heading.style.fontSize = "24px";
-    para.style.display = "block";
-    input.value = "";
-});
+
+    let birthDate = new Date(dob);
+    let today = new Date();
+
+    let years = today.getFullYear() - birthDate.getFullYear();
+    let months = today.getMonth() - birthDate.getMonth();
+    let days = today.getDate() - birthDate.getDate();
+
+    if (days < 0) {
+        months--;
+        days += 30;
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    result.innerHTML =
+        "<b>Years:</b> " + years +
+        "<br><b>Months:</b> " + months +
+        "<br><b>Days:</b> " + days;
+}
